ce9b1f7bf71915491f5e713bfe71cce763605524
hjm259775-a11y <hjm259775@gmail.com>
2026-05-19 23:19:01 +0800

    5.19

--- 1.cpp
+++ 1.cpp
@@ -1,28 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-//类模板
+//类模板与函数模板区别主要有两点:
+//1.类模板没有自动类型推导的使用方式
+//2.类模板在模板参数列表中可以有默认参数
+//
 template<class NAME,class AGE>
 class myn
 {
 public:
-    myn(NAME n,AGE a)
-    {
-        this->name=n;
-        this->age=a;
-    }
-
-    void shuchu()
-    {
-        cout<<this->name<<" "<<this->age<<endl;
-    }
-    NAME name;
-    AGE age;
+    
+    string name;
+    int age;
 };
 
 void ceshi1()
 {
-    myn<string,int> p1("leziren",18);
-    p1.shuchu();
+
 }
 
 int main()
