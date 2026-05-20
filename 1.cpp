71cffd8c9d0a605825024a7ac3b05b6ecc836f2d
hjm259775-a11y <hjm259775@gmail.com>
2026-05-20 23:10:32 +0800

    5.20

--- 1.cpp
+++ 1.cpp
@@ -3,18 +3,19 @@ using namespace std;
 //类模板与函数模板区别主要有两点:
 //1.类模板没有自动类型推导的使用方式
 //2.类模板在模板参数列表中可以有默认参数
-//
+
 template<class NAME,class AGE>
 class myn
 {
 public:
-    
+    myn();
     string name;
     int age;
 };
 
 void ceshi1()
 {
+    myn p1("leziren",18);
 
 }
 
